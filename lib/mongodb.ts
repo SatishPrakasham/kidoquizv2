3417ba6d676716be8c55ed838ddf1d81373a2f29
SatishPrakasham <112928636+SatishPrakasham@users.noreply.github.com>
2025-03-17 17:22:33 +0800

    redepolyment

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,17 +1,23 @@
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import dotenv from 'dotenv';
 
 dotenv.config(); // Load environment variables
 
 const uri = process.env.MONGODB_URI as string;
 
 if (!uri) {
-    throw new Error("⚠️ MONGODB_URI is not defined in .env file");
+    throw new Error("🚨 MONGODB_URI is not defined in .env file!");
 }
 
-const options = {};
+const options = {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+};
 
-// Define a global type to avoid TypeScript error
+// Declare a global variable for clientPromise
 declare global {
     var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
@@ -19,11 +25,11 @@ declare global {
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-// Ensure we reuse the database connection in development mode
 if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
 }
+
 clientPromise = global._mongoClientPromise;
 
 export default clientPromise;
